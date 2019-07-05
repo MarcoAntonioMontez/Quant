7cc8a1ede2df8d5283eba680ab5118af95edb98b
MarcoAntonioMontez <marco.montez@ist.utl.pt>
2019-07-05 11:56:37 +0100

    Small updates

--- modules/test.py
+++ modules/test.py
@@ -3,20 +3,64 @@ import pandas as pd
 import numpy as np
 import sys,os
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
+import plotly
+from plotly.offline import iplot
+import plotly.graph_objs as go
+plotly.offline.init_notebook_mode()
+from modules import logs_manager as logs
 from modules import data_manager as dm
 from modules import technical_manager as tm
-import plotly
-# from plotly.offline import iplot
-# import plotly.graph_objs as go
-# plotly.offline.init_notebook_mode()
+from modules import visualization_manager as vm
+from modules.UserInput import UserInput
+from modules.Trader import Trader
+from modules.Order import Order
+from modules.Statistics import Statistics
+from modules import ga
+pd.options.display.max_rows = 200
 
-data_path = '../data'
+data_path = '../data/'
 prices = '21_sample_2005_2016.csv'
+processed_prices = 'pre_processed_21_sample.csv'
+price_field = 'Close'
+sp500_name = 'SP500_index_prices_2005-01-01_2018-12-31.csv'
 
-dataset = dm.load_csv(data_path + '/' + prices,1)
+sp500 = pd.read_csv(data_path + sp500_name,index_col=0)
+
+update_fields = True
+
+if update_fields:
+    prices = '21_sample_2005_2016.csv'
+    dataset = dm.load_csv(data_path + prices,1)
+    ratios = [{'ratio_name':'ema', 'parameter':200},
+#               {'ratio_name':'ema', 'parameter':100},
+#               {'ratio_name':'ema', 'parameter':50},
+#               {'ratio_name':'ema', 'parameter':30},
+#               {'ratio_name':'ema', 'parameter':20},
+#               {'ratio_name':'atr', 'parameter':50},
+#               {'ratio_name':'atr', 'parameter':30},
+#               {'ratio_name':'atr', 'parameter':25},
+              {'ratio_name':'atr', 'parameter':20},
+              {'ratio_name':'atr', 'parameter':14},
+#               {'ratio_name':'aroon', 'parameter':50},
+#               {'ratio_name':'aroon', 'parameter':30},
+#               {'ratio_name':'aroon', 'parameter':25},
+              {'ratio_name':'aroon', 'parameter':20},
+              {'ratio_name':'aroon', 'parameter':14},
+              {'ratio_name':'rsi', 'parameter':14},
+              {'ratio_name':'macd_diff', 'parameter':[12,26,9]},
+              {'ratio_name':'cmf', 'parameter':20},
+              {'ratio_name':'cmo', 'parameter':14},
+              {'ratio_name':'mfi', 'parameter':14}
+             ]
+    ratio_names = ['ema','sma']
+    periods = [20,30,50,100,200]
+    tm.add_ma(ratios,ratio_names,periods)
+    dataset = tm.preprocess_table(dataset, ratios,price_field)
+    dataset.to_csv(data_path + processed_prices)
+else:
+    dataset = dm.load_csv(data_path + processed_prices,1)
 
 print(dataset.head())
-
 
 
 # # headers = dataset.columns.get_level_values(0)
